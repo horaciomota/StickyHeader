72dcc2d801ac89b4678688a1ceaf96b8648e5e03
Horacio Mota <horaciomota@Horacios-MacBook-Pro.local>
2023-10-07 03:47:13 -0700

    first commit

---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by Horacio Mota on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
